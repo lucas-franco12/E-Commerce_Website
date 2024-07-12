138c25715c02248baeb12676243a82a53bcb5f0d
aleLovadina <alelova03@gmail.com>
2024-07-11 21:49:16 -0400

    change package.json

--- backend/routes/sellers.js
+++ backend/routes/sellers.js
@@ -3,7 +3,7 @@ const {verifyTokenAndAuthorization, verifyTokenAndAdmin} = require('./verifyToke
 const User = require('../models/Seller');
 
 //Update
-router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
+router.put('/:id', async (req, res) => {
     if(req.body.password){
         req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString();
     }
